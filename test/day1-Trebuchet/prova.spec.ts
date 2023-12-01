f5674537e84d67757a9a03d8f14bb19a5e28a7ac
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2023-12-01 10:59:39 +0100

    recover calibration values at the edges

--- test/day1-Trebuchet/prova.spec.ts
+++ test/day1-Trebuchet/prova.spec.ts
@@ -1,3 +1,26 @@
-test("prova", () => {
-    expect(2+2).toBe(4)
+test.skip("solve provided example", () => {
+    const input = "1abc2\n" +
+      "pqr3stu8vwx\n" +
+      "a1b2c3d4e5f\n" +
+      "treb7uchet\n"
+
+    const sum = calibrationValuesSum(input)
+
+    expect(sum).toBe(142)
 })
+
+describe("recover calibration value from single row", () => {
+  test("numbers at the edges", () => {
+    const value = recoverCalibrationValueFromSingleRow("1abc2")
+
+    expect(value).toBe(12)
+  })
+})
+
+function calibrationValuesSum(input: string): number {
+  throw new Error("Function not implemented.")
+}
+
+function recoverCalibrationValueFromSingleRow(input: string) {
+  return 12
+}
