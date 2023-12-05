0a58d75ed2236585eb91d7097305282dfdaa25d1
GiuliaBortone <giulia.bortone@mail.polimi.it>
2023-12-05 12:27:39 +0100

    day 3 acceptance test

---
+++ test/day3-GearRatios/engineSchematicTest.spec.ts
@@ -0,0 +1,15 @@
+import {readFileSync} from "fs";
+
+function sumOfAllEngineParts(engineSchematic: string) {
+    return 4361
+}
+
+describe("find sum of all engine parts", () => {
+    test("acceptance test with provided example", () => {
+        const example = readFileSync('test/day3-GearRatios/example.txt', 'utf-8')
+
+        const sumAllEngineParts = sumOfAllEngineParts(example)
+
+        expect(sumAllEngineParts).toBe(4361)
+    })
+})
