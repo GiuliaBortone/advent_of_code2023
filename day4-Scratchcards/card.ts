00efa0b313de8aba5bdf41d0f58239ac0317f7e1
GiuliaBortone <giulia.bortone@mail.polimi.it>
2024-01-15 11:01:48 +0100

    card can find points for 2 matching numbers

--- day4-Scratchcards/card.ts
+++ day4-Scratchcards/card.ts
@@ -1,15 +1,27 @@
 export class Card {
-    private readonly winningNumbers: number[];
-    private readonly cardNumbers: number[];
+    private readonly winningNumbers: number[]
+    private readonly cardNumbers: number[]
 
     public constructor(winningNumbers: number[], cardNumbers: number[]){
-        this.winningNumbers = winningNumbers;
-        this.cardNumbers = cardNumbers;
+        this.winningNumbers = winningNumbers
+        this.cardNumbers = cardNumbers
     }
 
     public points() {
-        if (this.cardNumbers[0] == this.winningNumbers[0])
-            return 1
-        else return 0
+        let commonNumbers = this.findCommonNumbersQuantity()
+        return this.pointCalculator(commonNumbers);
+    }
+
+    private pointCalculator(commonNumbers: number) {
+        if (commonNumbers == 0) return 0
+        return Math.pow(2, commonNumbers - 1)
+    }
+
+    private findCommonNumbersQuantity() {
+        let commonNumbers: number = 0
+        this.cardNumbers.forEach((number) => {
+            if (this.winningNumbers.includes(number)) commonNumbers++
+        })
+        return commonNumbers;
     }
 }
--- test/day4-Scratchcards/cardTest.spec.ts
+++ test/day4-Scratchcards/cardTest.spec.ts
@@ -1,11 +1,16 @@
 import {Card} from "../../day4-Scratchcards/card"
 
 test("card with no matching numbers should have zero points", () => {
-    const card: Card = new Card([1], [2]);
+    const card: Card = new Card([1], [2])
     expect(card.points()).toBe(0)
 })
 
 test("card with one matching number should have 1 point", () => {
-    const card: Card = new Card([1], [1]);
+    const card: Card = new Card([1], [1])
     expect(card.points()).toBe(1)
+})
+
+test("card with two matching numbers should return 2 points", () => {
+    const card: Card = new Card([1, 2], [2, 1])
+    expect(card.points()).toBe(2)
 })
